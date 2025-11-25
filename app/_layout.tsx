8de5dfaf80881e609058aa6570dfd63089a06f00
yoshimidevz <yoshimidev1@gmail.com>
2025-11-25 09:00:47 -0300

    chore: update dependencies and devDependencies in package.json; add esModuleInterop in tsconfig.json

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,3 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -6,9 +5,10 @@ import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
-// Impede o splash de sumir até carregar as fontes
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -23,32 +23,23 @@ export default function RootLayout() {
     }
   }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
+  if (!loaded) return null;
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        {/* Rota de login */}
         <Stack.Screen
           name="login"
-          options={{ title: 'Login', headerShown: false }}
+          options={{ headerShown: false }}
         />
-
-        {/* Rota de cadastro */}
         <Stack.Screen
           name="signUp"
-          options={{ title: 'Cadastro', headerShown: false }}
+          options={{ headerShown: false }}
         />
-
-        {/* Rota principal com abas */}
         <Stack.Screen
           name="(tabs)"
           options={{ headerShown: false }}
         />
-
-        {/* Página de erro padrão */}
         <Stack.Screen name="+not-found" />
       </Stack>
 
