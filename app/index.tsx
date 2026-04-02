d0c0059b96f05b086acf0e315869d004c9241022
yoshimidevz <yoshimidev1@gmail.com>
2026-04-02 18:47:17 -0300

    refactor: simplify redirection to signUp using Redirect component

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,5 @@
-import { useEffect } from 'react';
-import { useRouter } from 'expo-router';
+import { Redirect } from 'expo-router';
 
 export default function Index() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace('/signUp');
-  }, []);
-
-  return null;
+  return <Redirect href="/signUp" />;
 }
